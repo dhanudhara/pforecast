79bcbbba9500469b9577224284f19ff5c7c9c7d0
dhanu perera <dhanut@tuta.io>
2025-02-03 18:59:44 +0530

    feat: using PyMySQL connector to make db connection

--- file_extractor/app/tabulate/__init__.py
+++ file_extractor/app/tabulate/__init__.py
@@ -4,9 +4,9 @@ from pandas.core.frame import DataFrame
 
 DB_USER = 'root'
 DB_PSWD = 'password'
-HOST = 'db'
+HOST = 'mysqldb:3306'
 DB_NAME = 'plastic'
-DB_URL = 'mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format(
+DB_URL = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
     DB_USER,
     DB_PSWD,
     HOST,
@@ -16,4 +16,4 @@ engine = sqlalchemy.create_engine(DB_URL)
 
 
 def tabulate(df: DataFrame):
-    df.to_sql(con=engine, name='plastics', if_exists='replace')
+    df.to_sql(con=engine, name='plastics', if_exists='append', index=False)
